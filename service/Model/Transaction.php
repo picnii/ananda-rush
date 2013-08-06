ad1bff7a224a8cf63552df956bad8769a58589d2
thanakan <thanakan.tj@gmail.com>
2013-08-06 11:06:01 +0700

    update transaction

--- service/Model/Transaction.php
+++ service/Model/Transaction.php
@@ -5,22 +5,36 @@
 	"template_id"=> 2
   )
 */
+$id = 22;
 function createTransaction($unit_id, $template_id)
 {
-	$created_id = 22;
-	//
-	return $created_id;
+	$SQL  = "INSERT INTO tranfer_transaction(unit_payment_id,template_id)  VALUES ('$unit_id', '$template_id'); SELECT SCOPE_IDENTITY()";
+     $result = DB_query($connect,$SQL);
+    if($result){
+        sqlsrv_next_result($result); 
+        sqlsrv_fetch($result); 
+        $created_id = sqlsrv_get_field($result, 0); 
+        return $created_id;
+    }else{
+        return false;
+    }
 }
 
 function findTransactionById($id)
 {
-	return array(
-		'id'=>20,
-		'unit_payment_id'=>30,
-		'template_id'=>1,
-		'create_time'=>'2009-12-20',
-		'unit_payment_unit_id'=>30
-	);
+     $SQL  = "select * from tranfer_transaction where id = $id";
+	 $result = DB_query($connect,$SQL);
+	 $row = DB_fetch_array($result);
+     if($row > 0){
+        return array(
+            'id'=>$row["id"],
+            'unit_payment_id'=>$row["unit_payment_id"],
+            'template_id'=>$row["template_id"],
+            'create_time'=>$row["create_time"]
+        );
+    }else{
+       return false;
+    }
 }
 
 /*
@@ -30,14 +44,20 @@ function findTransactionById($id)
 function findTransaction($q)
 {
 
-	$sql = "SELECT * FROM transaction WHERE {$q}";
-	return array(
-		'id'=>20,
-		'unit_payment_id'=>30,
-		'template_id'=>1,
-		'create_time'=>'2009-12-20',
-		'unit_payment_unit_id'=>30
-	);
+	$sql = "SELECT * FROM tranfer_transaction WHERE where id = $q";
+    $result = DB_query($connect,$SQL);
+	 $row = DB_fetch_array($result);
+	if($row > 0){
+        return array(
+            'id'=>$row[0]["id"],
+            'unit_payment_id'=>$row[0]["unit_payment_id"],
+            'template_id'=>$row[0]["template_id"],
+            'create_time'=>$row[0]["create_time"],
+            'unit_payment_unit_id'=>$row[0]["unit_payment_unit_id"]
+        );
+    }else{
+       return false;
+    }
 }
 
 /*
