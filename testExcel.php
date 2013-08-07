ee20781cc3b16d4d189d60cc1831a4b1c9cc5490
picnii <sompop@picnii.me>
2013-08-07 15:54:50 +0700

    Add Test Excel

---
+++ testExcel.php
@@ -0,0 +1,82 @@
+<?php
+
+header('Content-type: application/msexcel');
+
+// It will be called downloaded.pdf
+header('Content-Disposition: attachment; filename="report.xls"');
+
+?>
+
+<html xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:x="urn:schemas-microsoft-com:office:excel"
+xmlns="http://www.w3.org/TR/REC-html40">
+ 
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252"/>
+<meta name=ProgId content=Excel.Sheet/>
+<meta name=Generator content="Microsoft Excel 11"/>
+ 
+<!--[if gte mso 9]><xml>
+ <x:excelworkbook>
+  <x:excelworksheets>
+   <x:excelworksheet>
+    <x:name>** WORKSHEET NAME **</x:name>
+    <x:worksheetoptions>
+     <x:selected></x:selected>
+     <x:freezepanes></x:freezepanes>
+     <x:frozennosplit></x:frozennosplit>
+     <x:splithorizontal>** FROZEN ROWS + 1 **</x:splithorizontal>
+     <x:toprowbottompane>** FROZEN ROWS + 1 **</x:toprowbottompane>
+     <x:splitvertical>** FROZEN COLUMNS + 1 **</x:splitvertical>
+     <x:leftcolumnrightpane>** FROZEN COLUMNS + 1**</x:leftcolumnrightpane>
+     <x:activepane>0</x:activepane>
+     <x:panes>
+      <x:pane>
+       <x:number>3</x:number>
+      </x:pane>
+      <x:pane>
+       <x:number>1</x:number>
+      </x:pane>
+      <x:pane>
+       <x:number>2</x:number>
+      </x:pane>
+      <x:pane>
+       <x:number>0</x:number>
+      </x:pane>
+     </x:panes>
+     <x:protectcontents>False</x:protectcontents>
+     <x:protectobjects>False</x:protectobjects>
+     <x:protectscenarios>False</x:protectscenarios>
+    </x:worksheetoptions>
+   </x:excelworksheet>
+  </x:excelworksheets>
+  <x:protectstructure>False</x:protectstructure>
+  <x:protectwindows>False</x:protectwindows>
+ </x:excelworkbook>
+</xml>< ![endif]-->
+ 
+</head>
+<body>
+ 
+<table>
+ 
+<thead>
+   ** TABLE HEADER ROWS GO HERE **
+   <tr>
+      <th>Header Cell</th>
+      ...
+   </tr>
+</thead>
+ 
+<tbody>
+   ** TABLE DATA ROWS GO HERE **
+   <tr>
+      <td>Data Cell</td>
+      ...
+   </tr>
+</tbody>
+ 
+</table>
+ 
+</body>
+</html>
