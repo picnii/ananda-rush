21619c33a47e7431527d274112c1d8bfa04e1d58
thanakan <thanakan.tj@gmail.com>
2013-08-08 15:54:55 +0700

    แก้transaction

--- service/RequestManager/Testing.php
+++ service/RequestManager/Testing.php
@@ -7,6 +7,24 @@ if($_GET['action'] == 'test')
 {
 
 	//do test here
+    function findTransactionById()
+{           $id = 15;
+     $SQL  = "select * from tranfer_transaction where id = $id";
+    print_r($GLOBALS['connect']);
+	 //$result = DB_query($GLOBALS['connect'],$SQL);
+	/*  $row = DB_fetch_array($result);
+     if($row > 0){
+        return array(
+            'id'=>$row["id"],
+            'unit_payment_id'=>$row["unit_payment_id"],
+            'template_id'=>$row["template_id"],
+            'create_time'=>$row["create_time"]
+        );
+    }else{
+       return false;
+    } */
+}
+print_r($GLOBALS['connect']);
 
 
 
